7fc09678488834332f96c28867bf9d7cd53e8bcb
mikola-s <suharev.nv@gmail.com>
2019-09-23 12:47:19 +0300

    add_description

--- lesson_02/lesson_02_lvl_01_2.py
+++ lesson_02/lesson_02_lvl_01_2.py
@@ -1,3 +1,7 @@
+##Домашнее задание урок 2. Первый уровень ("if"):
+##Набрать все примеры посимвольно и заставить их работать,
+##разобраться в их работе.
+
 from random import choice
 test = choice([True, False])
 result = 'Test is True' if test else 'Test is False'
