b2faeadc73ec8309ed2aecee2fc21615a41c1d47
mikola-s <suharev.nv@gmail.com>
2019-09-23 09:42:14 +0300

    optimization_data_file_generator

--- lesson_03/data_file_generator_for_lsn_03_ex_02.py
+++ lesson_03/data_file_generator_for_lsn_03_ex_02.py
@@ -4,6 +4,6 @@ with open('./input_data_file_for_fizz_buzz.txt', 'w') as data_file:
     for counter in range(20):
         fizz = randint(2,10)
         buzz = fizz + randint(1,10)
-        end_number = randint(fizz if fizz > buzz else buzz, 100)
+        end_number = randint(buzz, 100)
         data_file.write(str(fizz) + ' ' + str(buzz) + ' ' + str(end_number))
         data_file.write(linesep if counter != 19 else '')
