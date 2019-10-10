5621f3f2fb8ca4a85aaef9d7f4b535f5cd88df1c
mikola-s <suharev.nv@gmail.com>
2019-10-10 16:22:20 +0300

    change in lesson 05 to PEP8

--- lesson_05/lesson_05_ex_01_3_practice.py
+++ lesson_05/lesson_05_ex_01_3_practice.py
@@ -1,19 +1,22 @@
-##Задание 3
-##Вспоминаем работу с файлом. Есть файл, в котором много
-##англоязычных текстовых строк. Считаем частоту встретившихся
-##слов в файле, но через функции и map, без единого цикла!
+# Задание 3
+# Вспоминаем работу с файлом. Есть файл, в котором много
+# англоязычных текстовых строк. Считаем частоту встретившихся
+# слов в файле, но через функции и map, без единого цикла!
 
 from re import sub
+
 
 def cleaner(string):
     return sub(r"[^\w']", "", string)
 
+
 def counter(item):
     rezult_dict[item] += 1
 
+
 def printing(item):
     print(rezult_dict[item], '\t', item)
-    
+
 
 with open('text_for_ex_01_3_practice.txt', 'r') as story:
     story_list = list(map(cleaner, story.read().split()))
