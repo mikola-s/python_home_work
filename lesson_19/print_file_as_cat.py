34c3e473a02367a29b3bd7a80ddd03b97c1c4a68
mikola-s <suharev.nv@gmail.com>
2019-10-30 21:54:39 +0200

    release print_file_as_cat.py

--- lesson_19/print_file_as_cat.py
+++ lesson_19/print_file_as_cat.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 
-def processing_input(input_str=str):
+def processing_input(input_str):
     """ проверяет введенный путь и имя файла
 
     проверяет есть ли в path_file путь и является ли он путем к файлу
@@ -15,16 +15,24 @@ def processing_input(input_str=str):
     """
 
     if os.path.exists(input_str):
-        return input_str if os.path.isfile(input_str) else print('\nОшибка:', input_str, '-- не являеться файлом\n')
+        if os.path.isfile(input_str):
+            return input_str
+        else:
+            print('\nОшибка:', input_str, '-- не являеться файлом\n')
+    elif input_str == '--help':
+        print(print_file_as_cat.__doc__)
     else:
         print('\nОшибка:', input_str, '-- неверный путь\n')
 
+    print('введите ./print_file_as_cat.py --help, чтобы получить дополнительную информацию\n')
+    sys.exit()
 
-def open_and_read_file(path_file=str):
+
+def open_and_read_file(path_file):
     """
     открывает файл на чтение и считывает его построчно в список
 
-    :param path_file: строка содержащая путь к файлу и имя файла
+    :param path_file: строка содержащая [<путь к файлу>]<имя файла>
     :return: список со строками файла
 
     """
@@ -35,33 +43,35 @@ def open_and_read_file(path_file=str):
         print('IOError не удалось обработать файл --', path_file)
 
 
-def print_file_in_terminal(list_of_file=list):
+def print_file(list_of_file):
     """
     выводит элементы списка в терминал
 
-    :param list_of_file: список
+    :param list_of_file: список содержащий строки заданного файла
 
     """
-    return [print(line) for line in list_of_file]
+    return [print(line, end='') for line in list_of_file]
 
 
-def print_file_as_cat(argument):
-    """Выводит содержимое файла в терминал
+def print_file_as_cat(argument=str):
+    """
+    Выводит содержимое файла в терминал
 
     Использование:  $ python3 print_file_as_cat.py [<пусть к файлу>]<имя файла>
               или   $ ./print_file_as_cat.py [<путь к файлу>]<имя файла>
 
     Реализует программу cat в python 3.7.
     Выводит в терминал содержимое файла с именем указанным в [<путь к файлу>]<имя файла>
+
+    :param argument: [<путь к файлу>]<имя файла>
+
     Если в [<путь к файлу>]<имя файла> есть пробелы их надо писать в кавычках (")
     Пример: "c:/Program Files/test.txt" "/home/dir with space/file name with space.txt"
 
     <путь к файлу>  может быть абсолютным или относительным
                     можно не указывать если файл находится в текущей папке
 
-    Если параметр [<путь к файлу>]<имя файла> не указан catch
-
-    Если аргументы не передаются программа ничего не выводит
+    Если параметр [<путь к файлу>]<имя файла> не указан выводит print_file_as_cat.__doc__
 
     Примеры использования:
     $ python3 print_file_as_cat.py example_files/example_file_1.txt
@@ -73,19 +83,13 @@ def print_file_as_cat(argument):
     #пример файла python
     print('Мотурначка!')
 
-    :param argument: [<путь к файлу>]<имя файла>
-    :return:
-
     """
-    return open_and_read_file(processing_input(argument)) if argument else False
+
+    print_file(open_and_read_file(processing_input(argument)))
 
 
 if __name__ == "__main__":
-    path_file = './example_files/example_file_1.txt'
-    # path_file = './color_print_files_as_cat.py'
-    # path_file = ''
-    # path_file = None
-
-    # processing_input(path_file)
-    print_file_as_cat(path_file)
-    # print_file_as_cat(sys.argv[1])
+    if len(sys.argv) > 1:
+        print_file_as_cat(sys.argv[1])
+    else:  # если аргументы не переданы
+        print('\nвведите ./print_file_as_cat.py --help, чтобы дополнительную информацию\n')
